9a088af955349893db084926a2bfffd827dc31e5
ritik09z <ritikkanthiya@gmail.com>
2023-12-08 13:08:33 +0530

    list

---
+++ arrey.js
@@ -0,0 +1,12 @@
+var list=['one','two','three','four']
+console.log(list)
+list.push('five')
+console.log(list)
+var check=list.length
+console.log(check)
+// list.pop()
+// console.log(list)
+
+for(i=0;i<list.length;i++){
+    console.log(list[i])
+}
