d0f063a5fe08eadc209c5affeb44bbc02c7f298a
ritik09z <ritikkanthiya@gmail.com>
2023-12-08 12:48:06 +0530

    java

---
+++ terniaryOP.js
@@ -0,0 +1,17 @@
+// let n=11;
+// let message;
+
+// // using if-else statement to check if the number is even or odd
+// if(n % 2== 0){
+//     message =("even")
+// }
+// else{
+//     message=("odd")
+// }
+// console.log(message)
+
+// using the ternery operators
+let number=11
+let secondnumber = number%2 ==0? "even" : "odd" ;
+ 
+console.log(secondnumber)
