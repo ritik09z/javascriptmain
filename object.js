391f3722abe806d020ea71158924129c9f90ec72
ritik09z <ritikkanthiya@gmail.com>
2023-12-08 13:31:23 +0530

    objectOP

--- object.js
+++ object.js
@@ -3,7 +3,20 @@ var number={
     age : 17,
     address : "indore"
 }
-console.log(number)
-console.log(number.age)
-console.log(number.name)
-console.log(number.address)
+// console.log(number)
+// console.log(number.age)
+// console.log(number.name)
+// console.log(number.address)
+
+var person ={
+     name : "ritik",
+     class : 11,
+     address :"indore",
+     hobbies :["coding","cricket","foothball"]
+     
+}
+console.log(person)
+
+var arr =[];
+arr.push(person)
+console.log(arr)
