e13f2e2fcb1de475e24dc84944e905389c81d8ad
ritik09z <ritikkanthiya@gmail.com>
2023-12-11 13:47:08 +0530

    foreach loop

---
+++ foreach.js
@@ -0,0 +1,12 @@
+var list=[2,31,4,13,4,41,4,3]
+
+list.forEach((value) =>{
+    console.log(value)
+
+})
+
+var one=["ritik","DJQOJFQOE","wudhquiwdh"]
+
+one.forEach((Element) =>{
+    console.log(Element)
+})
