e010a8b16a0b8e7eeb23fd11e35916363956031e
ritik09z <ritikkanthiya@gmail.com>
2023-12-13 13:13:12 +0530

    for map and for filter

---
+++ formap.js
@@ -0,0 +1,13 @@
+// var a=[1,2,4,5,6]
+// a.map((b)=>{
+//     console.log(b*3)
+// })
+
+// function(filter)
+
+var a=[1,2,34,5,5,6,3,3,23]
+var result =a.filter((v)=>{
+    return v%2==0
+
+})
+console.log(result)
